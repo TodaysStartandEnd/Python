88ecfe6dfb77c43a15dca8d57541b44f3a3ef5c6
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-05-21 14:53:14 +0000

    function(include return)

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-##function
-def hello_world(year = 2024, name = "Sunny") :
-  print("hello ",name)
-  print("happy ", year , "year")
+##function(include return)
+##return을 통해 함수에서의 값을 다른 함수로 넘겨##(반환해) 줄 수 있다.
+def input_name(name):
+  return name
 
-hello_world()
-hello_world(2025)
-hello_world(name="Cloudy")
-hello_world(2025,"Cloudy")
+
+def input_age(age):
+  return age
+
+
+def input_number_no_010(number):
+  fullnumber = "010" + number
+  return fullnumber
+
+##f" "안에 문자열과 {변수}구조를 연결해 ##string(문자열) format을 만들어 줄 수 있다.
+def info(name, age, number):
+  print(f"이름 : {name} \n나이 : {age} \n전화번호 : {number}")
+
+
+info(input_name("Sunny"), input_age(100), input_number_no_010("12345678"))
