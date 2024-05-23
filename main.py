85320dfc04618300a133842bdf4390dd511fcda2
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-05-23 14:49:10 +0000

    Standard Library && and, or

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-## control statement : if, elif, else
-## elif and else may or may not be used. 
+## Standard Library && and, or
+## https://docs.python.org/3/library/index.html
 
-score = int(input("Enter your score: "))
-if score >= 90:
-  print("A+")
-elif score >= 80:
-  print("A")
-elif score >= 70:
-  print("B+")
-elif score >= 60:
-  print("B")
-elif score >= 50:
-  print("C+")
-elif score >= 40:
-  print("C")
-else :
-  print("F")
+from random import randint
+
+print("roll the dice!");
+dice = randint(0, 7)
+if dice > 0 and dice  < 7 :
+  print("you roll a ",dice)
+elif dice == 0 or dice == 7 :
+  print(dice, " is an invaid value in this dice")
