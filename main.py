2ae1c15e99a4e04357618ca1ee240446cfe699bb
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-05-26 14:52:52 +0000

    list, tuple, dictionary

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-## Method
-## = data+function 
+## list, tuple, dictionary
 
-moto = "tAke rIsks"
+#list
+score = [10,20,30,40,50,"A"]
+print(score.append("A+"))
+print(score.insert(2,25))
+print(score.remove(50))
+print(score[-1])
 
-print(moto.capitalize())
-print(moto.upper())
-print(moto.lower())
-print(moto.find("r"))
-print(moto.replace("tAke","take"))
+#tuple(*immutable)
+dumbbell_weight = (10,20,30,10)
+print(dumbbell_weight.index(10))
+print(dumbbell_weight.count(10))
 
+#dictionary
+student = {
+  "name": "Sunny",
+  "age": 100,
+  "is_hungry": True,
+   1: "one" ## checking integer key is available
+}
+
+print(student.get("name"))
+print(student[1])
+student.update(is_hungry=False)
+print(student)
+print(student.pop("age"))
+print(student)
