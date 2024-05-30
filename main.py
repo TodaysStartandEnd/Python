c07d9b05ea3c2d8750763e1f63e032ac35f60446
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-05-30 14:54:54 +0000

    Inheritance

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-## Class 
-## method + variable
-## 1. method(functions in class) : The first parameter of the method points to the address of the object itself.
+## Inheritance
+##The parent class passes on methods and data to the child class.
+##If a child class wants to use the method of the parent class, it uses super().
 
 class Person :
-  def __init__(self) :
-      print (self) ##<__main__.Person object at 0x7901da58ebf0>
-      self.name = "Sunny"
-      self.age = 100
-      
+  def __init__(self, name, age) :
+      self.name = name 
+      self.age = age
+    
+  def say_hello(self) :
+      print("hello")
 
-Doctor = Person()
-print(Doctor) ## <__main__.Person object at 0x7901da58ebf0>
-print(Doctor.name); ##Sunny
-print(Doctor.age);##100
+  def add_age(self, year) :
+      self.age += year
+
+class Timetraveler(Person) :
+  def __init__(self, name, age):
+   super().__init__(name, age)
+    
+  def subtract_age(self, year) :
+      self.age -= year
+
+class Programmer(Person) :
+  def __init__(self, name, age, language) :
+    self.name = name 
+    self.age = age
+    self.language = language
+
+Sunny = Timetraveler("Sunny", 100)
+Sunny.subtract_age(10)
+print(Sunny.age)
+
+Cloudy = Programmer("Cloudy", 50, "Python")
+print(Cloudy.language)
+
+
 
 
 
