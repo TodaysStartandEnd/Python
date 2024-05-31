d65bf46fba80003d7e3046dad082d27ad496081b
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-05-31 14:55:37 +0000

    WebScraper1 - request content

--- main.py
+++ main.py
@@ -1,39 +1,9 @@
-## Inheritance
-##The parent class passes on methods and data to the child class.
-##If a child class wants to use the method of the parent class, it uses super().
+## WebScraper1 - request content
+## python can retrieve the content of the corresponding URL through the response to the request response. 
+import requests
 
-class Person :
-  def __init__(self, name, age) :
-      self.name = name 
-      self.age = age
-    
-  def say_hello(self) :
-      print("hello")
+url = "https://www.naver.com"
 
-  def add_age(self, year) :
-      self.age += year
-
-class Timetraveler(Person) :
-  def __init__(self, name, age):
-   super().__init__(name, age)
-    
-  def subtract_age(self, year) :
-      self.age -= year
-
-class Programmer(Person) :
-  def __init__(self, name, age, language) :
-    self.name = name 
-    self.age = age
-    self.language = language
-
-Sunny = Timetraveler("Sunny", 100)
-Sunny.subtract_age(10)
-print(Sunny.age)
-
-Cloudy = Programmer("Cloudy", 50, "Python")
-print(Cloudy.language)
-
-
-
-
+response = requests.get(url)
+print(response.content)
 
