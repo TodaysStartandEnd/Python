04e45a18b9db6ced47bc64e014b43440cf75f55c
TodaysStartandEnd <163544032+TodaysStartandEnd@users.noreply.github.com>
2024-06-03 13:53:50 +0000

    WebScraper3 - BeautifulSoup(html parsing library) : extraction text

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-## WebScraper2 - BeautifulSoup(html parsing library)
+## WebScraper3 - BeautifulSoup(html parsing library) : extraction text
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,7 +8,15 @@ response = requests.get(url)
 ##print(response.content)
 
 soup = BeautifulSoup(response.content, 'html.parser')
-nomadcourse_name = soup.find('div',class_="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-y-5 gap-x-10 pb-10").find_all('h3')
+nomadcourse_names = soup.find('div',class_="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-y-5 gap-x-10 pb-10")
+
+idx = 1;
+for name in nomadcourse_names :
+  title = name.find("h3", class_ ="text-xl overflow-hidden").text
+  print("-----------------------------------------------")
+  print(idx," ",title)
+  idx += 1
+  
 
 
-print(nomadcourse_name)
+
